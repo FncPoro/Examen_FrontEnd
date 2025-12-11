0c9cec83c4185af68a1ea89a7cfa5a45d806eb30
Adrian <153190497+FncPoro@users.noreply.github.com>
2025-12-11 20:32:24 +0100

    Arreglos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 //PAGINA PRINCIPAL ("localhost:3000/") LAS RUTAS FUNCIONAN POR CARPETAS DENTRO DE APP, app/calendarios = localhost:3000/calendarios
 //CAMBIAR PARA EXAMEN
-import CalendarHome from "@/app/home/page";
 
 export default function Home() {
   return (
     <>
-       <CalendarHome></CalendarHome>
     </>
   );
 }
